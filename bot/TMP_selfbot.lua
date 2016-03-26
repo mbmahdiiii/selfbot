dd238b323ec71a1d2b4e25d8ef0cdc9b61409c53
mbmahdiiii <mbmahdi650@gmail.com>
2016-03-27 01:33:39 +0430

    Update TMP_selfbot.lua

--- bot/TMP_selfbot.lua
+++ bot/TMP_selfbot.lua
@@ -225,30 +225,12 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {PutYourID(BOTID),tonumber(our_id)},--Sudo users
+    sudo_users = {115887468(115887468),tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[mb
 
-https://github.com/SEEDTEAM/TeleSeed
 
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
 ]],
     help_text_realm = [[
 Realm Commands:
